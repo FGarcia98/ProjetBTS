5cea4b3e395129ba8b2d57c67e2aa24b41e38a1b
FLEMAL <55393709+Rom-1-code@users.noreply.github.com>
2020-09-05 19:54:34 +0200

    First commit

---
+++ class/user.php
@@ -0,0 +1,36 @@
+<?php
+class user{
+
+    //Propiétés
+    private $_identifiant;
+    private $_mdp;
+
+    //Methodes
+    public function bdd()
+    {
+        try {
+            $bdd = new PDO('mysql:host=localhost; dbname=projetgps; charset=utf8', 'root', 'root');
+        } catch (Exception $erreur) {
+            echo 'Erreur : ' . $erreur->getMessage();
+        }
+        return $bdd;
+    }
+
+    public function Connexion($identifiant, $mdp , $bdd)
+    {
+            $requser = $bdd->prepare('SELECT * FROM user WHERE "'.$identifiant.'"=`identifiant` && "'.$mdp.'"=`mdp');
+            $requser->execute(array($identifiant, $mdp));
+            $userexist = $requser->rowCount();
+            if ($userexist == 1) {
+                $userinfo = $requser->fetch();
+                $_SESSION['identifiant'] = $userinfo['identifiant'];
+                echo "Vous être connecté en tant que " . $userinfo['identifiant'] . ".";
+            } else {
+             echo "Identifiant ou mot de passe incorrect !";
+            }
+    }
+
+
+}
+
+?>
