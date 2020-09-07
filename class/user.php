cefeeeb04e7f9aee13b1a20d1f9fd6453c7ceee7
Rom-1-code <rflemal@la-providence.net>
2020-09-07 18:09:05 +0200

    commit 18:00 07/09/2020

--- class/user.php
+++ class/user.php
@@ -16,7 +16,7 @@ class user{
         return $bdd;
     }
 
-    public function Connexion($identifiant, $mdp , $bdd)
+    public function Connexion($identifiant, $mdp , $bdd) // Romain FLEMAL
     {
             $requser = $bdd->prepare('SELECT * FROM user WHERE "'.$identifiant.'"=`identifiant` && "'.$mdp.'"=`mdp');
             $requser->execute(array($identifiant, $mdp));
@@ -25,9 +25,38 @@ class user{
                 $userinfo = $requser->fetch();
                 $_SESSION['identifiant'] = $userinfo['identifiant'];
                 echo "Vous être connecté en tant que " . $userinfo['identifiant'] . ".";
+
+                if($userinfo['isadmin']==1) //admin
+                {
+                    echo "user ou admin?";
+                }
             } else {
              echo "Identifiant ou mot de passe incorrect !";
             }
+    }
+
+    public function Autorisation($identifiant, $mdp) // Florian Garcia 
+    {
+        
+
+
+
+    }
+
+    public function Modification_user($identifiant, $mdp) // Florian Garcia
+    {
+
+
+
+
+    }
+
+    public function Suppression_user($identifiant, $mdp) // Romain FLEMAL
+    {
+
+
+
+
     }
 
 
